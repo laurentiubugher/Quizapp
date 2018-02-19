526ea4e840a971d7250a2aad4ccd1d46ae0132a0
Laurentiu Bugher <thebryx>
2018-02-19 15:00:36 +0000

    Improved Version

--- app/src/main/java/com/example/android/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.quizapp;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,9 +17,9 @@ public class QuizActivity extends AppCompatActivity {
     //Create variables
     String playerName;
     int score = 0;
-    private RadioButton q1C, q2B, q7D;
-    private CheckBox q5CB1, q5CB2, q5CB3, q5CB4, q6CB1, q6CB2, q6CB3, q6CB4, q8CB1, q8CB2, q8CB3, q8CB4;
-    private EditText q3eT, q4eT;
+    private RadioButton q1_rb_Tesla, q2_rb_i8, q7_rb_Lamborghini;
+    private CheckBox q5CheckBox_RollsRoyce, q5CheckBox_Bentley, q5CheckBox_manufacturedInEngland, q5CheckBox_manufacturedInGermany, q6CheckBox_Porsche, q6CheckBox_Audi, q6CheckBox_i8, q6CheckBox_R8, q8CheckBox_RangeRover, q8CheckBox_Volvo, q8CheckBox_Sport, q8CheckBox_SUV;
+    private EditText q3_EditText, q4_EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,85 +33,83 @@ public class QuizActivity extends AppCompatActivity {
 
         //This uses findViewById to assign the values to the global variables
         //Radio Buttons
-        q1C = findViewById(R.id.q1C);
-        q2B = findViewById(R.id.q2B);
-        q7D = findViewById(R.id.q7D);
+        q1_rb_Tesla = findViewById(R.id.q1_rb_Tesla);
+        q2_rb_i8 = findViewById(R.id.q2_rb_i8);
+        q7_rb_Lamborghini = findViewById(R.id.q7_rb_Lamborghini);
 
         //Check Boxes
-        q5CB1 = findViewById(R.id.q5CB1);
-        q5CB2 = findViewById(R.id.q5CB2);
-        q5CB3 = findViewById(R.id.q5CB3);
-        q5CB4 = findViewById(R.id.q5CB4);
-        q6CB1 = findViewById(R.id.q6CB1);
-        q6CB2 = findViewById(R.id.q6CB2);
-        q6CB3 = findViewById(R.id.q6CB3);
-        q6CB4 = findViewById(R.id.q6CB4);
-        q8CB1 = findViewById(R.id.q8CB1);
-        q8CB2 = findViewById(R.id.q8CB2);
-        q8CB3 = findViewById(R.id.q8CB3);
-        q8CB4 = findViewById(R.id.q8CB4);
+        q5CheckBox_RollsRoyce = findViewById(R.id.q5CheckBox_RollsRoyce);
+        q5CheckBox_Bentley = findViewById(R.id.q5CheckBox_Bentley);
+        q5CheckBox_manufacturedInEngland = findViewById(R.id.q5CheckBox_manufacturedInEngland);
+        q5CheckBox_manufacturedInGermany = findViewById(R.id.q5CheckBox_manufacturedInGermany);
+        q6CheckBox_Porsche = findViewById(R.id.q6CheckBox_Porsche);
+        q6CheckBox_Audi = findViewById(R.id.q6CheckBox_Audi);
+        q6CheckBox_i8 = findViewById(R.id.q6CheckBox_i8);
+        q6CheckBox_R8 = findViewById(R.id.q6CheckBox_R8);
+        q8CheckBox_RangeRover = findViewById(R.id.q8CheckBox_RangeRover);
+        q8CheckBox_Volvo = findViewById(R.id.q8CheckBox_Volvo);
+        q8CheckBox_Sport = findViewById(R.id.q8CheckBox_Sport);
+        q8CheckBox_SUV = findViewById(R.id.q8CheckBox_SUV);
 
         //Edit texts
-        q3eT = findViewById(R.id.q3_editText);
-        q4eT = findViewById(R.id.q4_editText);
+        q3_EditText = findViewById(R.id.q3_editText);
+        q4_EditText = findViewById(R.id.q4_editText);
     }
 
     //This method shows the Toast message, when the check answers button is pressed
     public void submitAnswers(View v) {
 
         //Value answer for question 1
-        if(q1C.isChecked()){
+        if(q1_rb_Tesla.isChecked()){
             score += 1;
         }
 
         //Value answer for question 2
-        if(q2B.isChecked()){
+        if(q2_rb_i8.isChecked()){
             score += 1;
         }
 
         //Value answer for question 3
-        String answerQ3 = q3eT.getText().toString();
+        String answerQ3 = q3_EditText.getText().toString();
         String correctAnswerQ3 = "Mercedes";
         if(answerQ3.matches(correctAnswerQ3)){
             score += 1;
         }
 
         //Value answer for question 4
-        String answerQ4 = q4eT.getText().toString();
+        String answerQ4 = q4_EditText.getText().toString();
         String correctAnswerQ4 = "Lamborghini";
         if(answerQ4.matches(correctAnswerQ4)){
             score += 1;
         }
 
         //Value answer for question 5
-        if(q5CB2.isChecked() && q5CB3.isChecked() && !q5CB1.isChecked() && !q5CB4.isChecked()) {
+        if(q5CheckBox_Bentley.isChecked() && q5CheckBox_manufacturedInEngland.isChecked() && !q5CheckBox_RollsRoyce.isChecked() && !q5CheckBox_manufacturedInGermany.isChecked()) {
             score += 1;
         }
 
         //Value answer for question 6
-        if(q6CB2.isChecked() && q6CB4.isChecked() && !q6CB1.isChecked() && !q6CB3.isChecked()) {
+        if(q6CheckBox_Audi.isChecked() && q6CheckBox_R8.isChecked() && !q6CheckBox_Porsche.isChecked() && !q6CheckBox_i8.isChecked()) {
             score += 1;
         }
 
         //Value answer for question 7
-        if(q7D.isChecked()){
+        if(q7_rb_Lamborghini.isChecked()){
             score += 1;
         }
 
         //Value answer for question 8
-        if(q8CB2.isChecked() && q8CB4.isChecked() && !q8CB1.isChecked() && !q8CB3.isChecked()){
+        if(q8CheckBox_Volvo.isChecked() && q8CheckBox_SUV.isChecked() && !q8CheckBox_RangeRover.isChecked() && !q8CheckBox_Sport.isChecked()){
             score += 1;
         }
 
         //This shows the toast message when the check answers button is pressed.
-        String congratsToast = "Congratulations, " + playerName + "!";
-        congratsToast += "\n\tYour score is " + score + " out of 8.";
-        congratsToast += "\n\t\t\t\t\tWell done!";
-        String tryAgain = "\t" + playerName + ", you failed!";
-        tryAgain += "\nYour score is " + score + " out of 8.";
-        tryAgain += "\n\t\t\t\t\tTry again!";
+        Resources resCongratsToast = getResources();
+        String congratsToast = resCongratsToast.getString(R.string.congratsToastMessage, playerName, score);
+        Resources resTryAgainToast = getResources();
+        String tryAgainToast = resTryAgainToast.getString(R.string.tryAgainToastMessage, playerName, score);
         if(score < 6) {
-            Toast.makeText(getApplicationContext(), tryAgain, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), tryAgainToast, Toast.LENGTH_LONG).show();
         }
         else {
             Toast.makeText(getApplicationContext(), congratsToast, Toast.LENGTH_LONG).show();
